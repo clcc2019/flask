0c8dfc9bee60f654c7eac15057677c951076884d
clcc2019 <dsong.cc@gmail.com>
2019-02-15 13:49:47 +0800

    修改前端UI

--- webapp.py
+++ webapp.py
@@ -4,22 +4,19 @@ from main import use_total, total, DB
 app = Flask(__name__)
 app.config.from_pyfile('settings.py')
 
-@app.route('/', methods=['POST','GET'])
+@app.route('/query', methods=['POST','GET'])
 def index():
     use = use_total.use()
     all = total.get_all()
-    db = DB.conndb(use, all)
-    db.db_insert()
+    #db = DB.conndb(use, all)
+    #db.db_insert()
     return render_template('index.html', data=[use, all])
-
 @app.route('/history', methods=['GET', 'POST'])
 def history():
-    db = DB.conndb()
-    query_data = db.query_db() # 查询结果列表
-    return render_template('history.html', data=query_data)
-
-
+    #db = DB.conndb()
+    #query_data = db.query_db() # 查询结果列表
+    return render_template('history.html',data=['暂时关闭功能'])
 if __name__=='__main__':
-    app.run(debug=True, host='0.0.0.0', port=809)
+    app.run(debug=False, host='0.0.0.0')
 #root@cc:/home/web#uwsgi -s /tmp/webapp.sock --manage-script-name --mount /home/web=webapp:app --daemonize=True
 
